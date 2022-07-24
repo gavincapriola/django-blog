3c70706c163f802f13b68c423b928cbb844a55d0
Gavin Capriola <gavincapriola@icloud.com>
2022-07-23 20:16:55 -0400

    handling routes

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the blog home.")
+
+
+def about(request):
+    return HttpResponse("This is the about page.")
