2e099067df6bc67baeebd0e1141f76d74a86208c
Gavin Capriola <gavincapriola@icloud.com>
2022-07-24 17:28:35 -0400

    create model post

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=512)
+    body = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post One',
-        'body': 'first',
-        'created_date': 'July 23, 2022'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post Two',
-        'body': 'second',
-        'created_date': 'July 23, 2022'
-    }
-]
+from .models import Post
 
 
 def index(request):
-    return render(request, 'blog/index.html', {'posts': posts})
+    return render(request, 'blog/index.html', {'posts': Post.objects.all()})
 
 
 def about(request):
