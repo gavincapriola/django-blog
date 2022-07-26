c3d2c9f0496590096a68fd9ddb4ea047c435b21f
Gavin Capriola <gavincapriola@icloud.com>
2022-07-26 11:19:31 -0400

    django crispy forms

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django Crispy Forms
+# https://django-crispy-forms.readthedocs.io/en/latest/install.html
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
