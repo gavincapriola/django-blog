ab2e2f4ec7fd613a463f9441578cafc515652ba5
Gavin Capriola <gavincapriola@icloud.com>
2022-07-26 11:17:23 -0400

    user registration

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            messages.success(
+                request, "Your account has been created! You're now able to log in")
+            form.save()
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
