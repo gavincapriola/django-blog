be99636c73814ba055db43ba8f04a3a27dbb2373
Gavin Capriola <gavincapriola@icloud.com>
2022-07-29 23:25:57 -0400

    list view, detail view

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
 from .models import Post
 
 
 def index(request):
     return render(request, 'blog/index.html', {'posts': Post.objects.all()})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-created_date']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
