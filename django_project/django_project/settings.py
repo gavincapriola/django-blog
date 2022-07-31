49387e54ff3d6a3490c5b7d063f1230c5464d42c
Gavin Capriola <gavincapriola@icloud.com>
2022-07-31 18:03:36 -0400

    password reset

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -135,3 +135,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''  # Enter your email here
+EMAIL_HOST_PASSWORD = ''  # Enter your password here
