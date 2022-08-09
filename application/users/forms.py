64950b6f47487dd23a080a207facd6d3d03f353d
Gavin Capriola <gavincapriola@icloud.com>
2022-08-09 14:18:27 -0400

    create clean_email func to make email unique

--- application/users/forms.py
+++ application/users/forms.py
@@ -1,13 +1,37 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils.html import mark_safe
 
 from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        label='Username',
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(
-        max_length=254, help_text='Required. Inform a valid email address.')
+        label='Email',
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(
+        help_text=mark_safe('<ul><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'),
+        label='Password',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(
+        help_text='Enter the same password as before, for verification.',
+        label='Confirm Password',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('This email address is already in use.')
+        return email
 
     class Meta:
         model = User
